ba0c218c63a36faced2a2c32f1ffea2415a58243
Programwithshubham <114507544+Programwithshubham@users.noreply.github.com>
2024-05-12 22:38:18 +0530

    Truthy and Falsy value
    1. In javascript all positive and negative value are Truthy value
    2.  falsy value in Javascript - 0 , ' ',undefined , null , NaN, !!0

---
+++ Trucy_falsy.js
@@ -0,0 +1,16 @@
+
+// In javascript all positive and negative value are Truthy value
+
+console.log(Boolean(-45));//true
+console.log(Boolean(2));//true
+
+
+//  following falsy value in Javascript
+console.log(Boolean(0));//false
+console.log(Boolean(-0));//false
+console.log(Boolean(''));//false
+console.log(Boolean(null));//false
+console.log(Boolean(NaN));//false
+console.log(Boolean(undefined));//false
+
+
