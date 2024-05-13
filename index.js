cfeb9104d9beece6196423bc719b927ac41ade57
Programwithshubham <114507544+Programwithshubham@users.noreply.github.com>
2024-05-13 22:22:43 +0530

    array methods

--- index.js
+++ index.js
@@ -1,21 +1,91 @@
-console.log(Math.floor(23.2));//23
-console.log(Math.ceil(23.2));//24
-console.log(34 ** 2); //1156
-console.log(Math.pow(2,6));//64
-console.log(Math.round(3442.2));//3442
-console.log(Math.round(3442.6));//3443
-console.log(Math.random());//0.7136114441368251
+// array and array method
 
 
-console.log(Math.floor(Math.random()*20));//16
-console.log(7/0);//infinity
-console.log(-7/0);//-infinity
-console.log(typeof Infinity);//number
-console.log(0/0);//NaN
+let arr1=["developer","tester","fornt end ","back end"];
+let arr2=["shubham","dhobe","patil","hi"];
+let arr3=a.fill("full stack developer")
 
-// Input from user 
 
- let width =prompt("Enter Number  and Rectangle and width:")
- let height =prompt("Enter Number  and Rectangle and Height:")
- 
-document.write(`Rectangle Height and width  : ${ width * height}`);
+
+filter
+
+let arr4=[1,23,4,35,5]
+let arr5=a.filter(test)
+function test(element,index,a){
+    return element>=10;
+}
+console.log(c);
+
+
+// push() :- add the last elemet of array
+let push1=[1,23,4,35,5]
+console.log(push1);
+
+let c=a.push(23,23)
+console.log(c)
+console.log(a)
+
+
+// pop :- remove the last element of array
+let pop1=[1,23,4,35,5]
+let pop2=a.pop()
+console.log(pop1)
+console.log(pop2)
+
+
+// reverse
+let a=[1,23,4,35,5]
+let b=a.reverse()
+console.log(a)
+
+
+// shift() :- remove the first elemet of array
+let shift1=[1,23,4,35,5]
+let shift2=a.shift()
+console.log(shift1)
+console.log(shift2)
+
+
+// unshift() :- add element in starting
+let unshift1=[1,23,4,35,5]
+let unshift2=a.unshift(127)
+console.log(unshift2)
+console.log(unshift1)
+
+
+
+
+// slice()
+let slice1=[1,23,4,35,5]
+let slice2=a.slice(1,4)
+console.log(slice1)
+console.log(slice2)
+
+
+// splice()
+let splice1=[1,23,4,35,5]
+let splice2=a.splice(1,2,23)
+console.log(splice1)
+console.log(splice2)
+
+
+
+// tostring() :- return array to string
+let num1=[1,23,4,35,5]
+let num2=a.toString()
+console.log(num1);
+console.log(num2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
